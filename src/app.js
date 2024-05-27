416b2d79145f55a6dbd1f7e03e1ec813271f645a
Mohamad Hajjar <69475046+M-Hajjar@users.noreply.github.com>
2024-05-28 01:42:10 +0200

    adding an input handeler and score holder

--- src/app.js
+++ src/app.js
@@ -7,4 +7,88 @@ const loadApp = () => {
   initWelcomePage();
 };
 
-window.addEventListener('load', loadApp);
+
+let currentQuestionIndex = quizData.currentQuestionIndex;
+
+const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const scoreElement = document.getElementById('score').querySelector('span');
+const continueButton = document.getElementById('continue-button');
+const resultContainer = document.getElementById('result-container');
+const resultElement = document.getElementById('result');
+const restartButton = document.getElementById('restart-button');
+
+let score = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+  startQuiz();
+});
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  resultContainer.classList.add('hidden');
+  questionContainer.classList.remove('hidden');
+  scoreElement.innerText = score;
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(quizData.questions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.text;
+  Object.keys(question.answers).forEach(key => {
+    const button = document.createElement('button');
+    button.innerText = question.answers[key];
+    button.classList.add('btn');
+    button.dataset.answer = key;
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const selectedAnswer = selectedButton.dataset.answer;
+  const correctAnswer = quizData.questions[currentQuestionIndex].correct;
+
+  if (selectedAnswer === correctAnswer) {
+    score++;
+    scoreElement.innerText = score;
+  }
+
+  if (currentQuestionIndex < quizData.questions.length - 1) {
+    currentQuestionIndex++;
+    setNextQuestion();
+  } else {
+    showResult();
+  }
+}
+
+function showResult() {
+  questionContainer.classList.add('hidden');
+  resultContainer.classList.remove('hidden');
+  resultElement.innerText = `Your score is ${score}`;
+}
+
+continueButton.addEventListener('click', () => {
+  if (currentQuestionIndex < quizData.questions.length - 1) {
+    currentQuestionIndex++;
+    setNextQuestion();
+  } else {
+    showResult();
+  }
+});
+
+restartButton.addEventListener('click', startQuiz);
+s
