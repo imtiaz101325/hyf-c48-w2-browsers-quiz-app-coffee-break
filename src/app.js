6263d46b30896679408728c418e97745e8f94137
YousrElmag <yousra3489@gmail.com>
2024-05-27 07:29:03 +0200

    welcomepage

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import { quizData } from './data.js';
 import { initWelcomePage } from './pages/welcomePage.js';
 
-const loadApp = () => {
-  quizData.currentQuestionIndex = 0;
 
-  initWelcomePage();
-};
 
-window.addEventListener('load', loadApp);
+
+
+
+
+
+// const loadApp = () => {
+//   quizData.currentQuestionIndex = 0;
+
+//   initWelcomePage();
+// };
+
+// window.addEventListener('load', loadApp);
--- src/views/welcomeView.js
+++ src/views/welcomeView.js
@@ -12,3 +12,25 @@ export const createWelcomeElement = () => {
   `;
   return element;
 };
+
+const QuizeArea=document.querySelector('.quizarea')
+const ExitButton=document.querySelector('.exit')
+const ContinueButton=document.querySelector('.continue')
+const container=document.querySelector('.container')
+ const StartButton=document.querySelector('.container button')
+ const guide=document.querySelector('.guide');
+ StartButton.addEventListener('click',()=>{
+  container.style.display='none';
+  guide.style.display='block'
+}) 
+ExitButton.addEventListener('click',()=>{
+  guide.style.display='none'
+  container.style.display='block'
+}) 
+ContinueButton.addEventListener('click',()=>{
+  guide.style.display='none'
+QuizeArea.style.display='block'
+document.body.style.background='#10002b'
+
+
+})
