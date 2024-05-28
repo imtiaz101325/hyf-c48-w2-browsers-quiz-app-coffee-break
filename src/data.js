79994e4463df21735ca4f879e4154ebfa4a326e8
YousrElmag <yousra3489@gmail.com>
2024-05-28 08:04:41 +0200

    add questions

--- src/data.js
+++ src/data.js
@@ -11,50 +11,118 @@
 
 export const quizData = {
   currentQuestionIndex: 0,
-  // the questions in the quiz
-  questions: [
-    {
-      text: 'What are the different ways to declare a JS variable?',
-      answers: {
-        a: 'constant, let, variable',
-        b: 'var, const, let, function',
-        c: 'var, let, const',
+  questions: 
+    [
+      {
+        "text": "A flashing red traffic light signifies that a driver should do what?",
+        "answers": {
+          "a": "stop",
+          "b": "speed up",
+          "c": "proceed with caution",
+          "d": "honk the horn"
+        },
+        "correct": "a",
+        "selected": null
       },
-      correct: 'c',
-      selected: null,
-      links: [
-        {
-          text: 'javascript.info',
-          href: 'https://javascript.info/variables',
+      {
+        "text": "A knish is traditionally stuffed with what filling?",
+        "answers": {
+          "a": "potato",
+          "b": "creamed corn",
+          "c": "lemon custard",
+          "d": "raspberry jelly"
         },
-        {
-          text: 'Tyler McGinnis',
-          href: 'https://ui.dev/var-let-const/',
-        },
-      ],
-    },
-    {
-      text: 'What does `typeof` do?',
-      answers: {
-        a: 'changes the type of a primitive value',
-        b: 'returns a string describing the type of a value',
-        c: 'determines if a value is primitive',
-        d: 'can tell the difference between arrays and objects',
+        "correct": "a",
+        "selected": null
       },
-      correct: 'b',
-      selected: null,
-      links: [
-        {
-          text: 'javascript.info',
-          href: 'https://javascript.info/types#type-typeof',
+      {
+        "text": "A pita is a type of what?",
+        "answers": {
+          "a": "fresh fruit",
+          "b": "flat bread",
+          "c": "French tart",
+          "d": "friend bean dip"
         },
-        {
-          text: 'MDN',
-          href:
-            'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof',
+        "correct": "b",
+        "selected": null
+      },
+      {
+        "text": "A portrait that comically exaggerates a person's physical traits is called a what?",
+        "answers": {
+          "a": "landscape",
+          "b": "caricature",
+          "c": "still life",
+          "d": "Impressionism"
         },
-      ],
-    },
-    // Add more questions here
-  ],
-};
+        "correct": "b",
+        "selected": null
+      },
+      {
+        "text": "A second-year college student is usually called a what?",
+        "answers": {
+          "a": "sophomore",
+          "b": "senior",
+          "c": "freshman",
+          "d": "junior"
+        },
+        "correct": "a",
+        "selected": null
+      },
+      {
+        "text": "A student who earns a J.D. can begin his or her career as a what?",
+        "answers": {
+          "a": "lawyer",
+          "b": "bricklayer",
+          "c": "doctor",
+          "d": "accountant"
+        },
+        "correct": "a",
+        "selected": null
+      },
+      {
+        "text": "A triptych is a work of art that is painted on how many panels?",
+        "answers": {
+          "a": "two",
+          "b": "three",
+          "c": "five",
+          "d": "eight"
+        },
+        "correct": "b",
+        "selected": null
+      },
+      {
+        "text": "According to a famous line from the existentialist play 'No Exit' what is hell?",
+        "answers": {
+          "a": "oneself",
+          "b": "other people",
+          "c": "little made large",
+          "d": "hued in green and blue"
+        },
+        "correct": "b",
+        "selected": null
+      },
+      {
+        "text": "According to a popular slogan, what state should people not 'mess with'?",
+        "answers": {
+          "a": "New York",
+          "b": "Texas",
+          "c": "Montana",
+          "d": "Rhode Island"
+        },
+        "correct": "b",
+        "selected": null
+      },
+      {
+        "text": "According to a Yale University study, what smell is the most recognizable to American adults?",
+        "answers": {
+          "a": "tuna",
+          "b": "laundry",
+          "c": "popcorn",
+          "d": "coffee"
+        },
+        "correct": "d",
+        "selected": null
+      },
+      
+    ]
+  } 
